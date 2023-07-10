9f078fc62822d709ad05f503194c70d729b41ac5
root <root@ctbl11sard01.cells.es>
2023-07-10 15:19:39 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="sardana_linkam",
-    version="1.0.3",
+    version="1.0.4",
     author="ALBA Controls Group",
     author_email="controls@cells.es",
     maintainer="Gabriel Jover-Manas",
